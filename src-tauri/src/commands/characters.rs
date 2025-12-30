aa757ac91ce288293279bb8c4bea8d9d6b70b6b8
snipereagle1 <me@snipereagle.xyz>
2025-12-29 23:00:31 -0700

    fix: update Character type to avoid typescript issues

--- src-tauri/src/commands/characters.rs
+++ src-tauri/src/commands/characters.rs
@@ -8,6 +8,8 @@ pub struct Character {
     pub character_id: i64,
     pub character_name: String,
     pub unallocated_sp: i64,
+    pub account_id: Option<i64>,
+    pub sort_order: i64,
 }
 
 impl From<db::Character> for Character {
@@ -16,6 +18,8 @@ impl From<db::Character> for Character {
             character_id: c.character_id,
             character_name: c.character_name,
             unallocated_sp: c.unallocated_sp,
+            account_id: c.account_id,
+            sort_order: c.sort_order,
         }
     }
 }
