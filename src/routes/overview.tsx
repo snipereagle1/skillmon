3f2bd70c9da1c0254c810e7933da1f2c5ca3dc7c
snipereagle1 <me@snipereagle.xyz>
2026-02-03 00:55:27 -0700

    fix: make overview scrollable

--- src/routes/overview.tsx
+++ src/routes/overview.tsx
@@ -1,12 +1,15 @@
 import { createFileRoute } from '@tanstack/react-router';
 
 import { OverviewTable } from '@/components/OverviewTable';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 function OverviewPage() {
   return (
-    <div className="container mx-auto p-4 space-y-6">
-      <OverviewTable />
-    </div>
+    <ScrollArea className="h-full">
+      <div className="container mx-auto p-4 space-y-6">
+        <OverviewTable />
+      </div>
+    </ScrollArea>
   );
 }
 
