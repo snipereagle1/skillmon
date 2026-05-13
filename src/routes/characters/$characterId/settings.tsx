ad33a88d45389db125b2a6111c7468ce5088048c
snipereagle1 <me@snipereagle.xyz>
2026-05-12 19:44:31 -0600

    refactor(settings): remove LocationDemo scaffold
    Predated the dedicated location tab; that tab now owns the UI.

--- src/routes/characters/$characterId/settings.tsx
+++ src/routes/characters/$characterId/settings.tsx
@@ -1,6 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router';
 
-import { LocationDemo } from '@/components/LocationDemo';
 import { NotificationSettings } from '@/components/NotificationSettings';
 
 function SettingsPage() {
@@ -8,9 +7,6 @@ function SettingsPage() {
   return (
     <div className="space-y-6">
       <NotificationSettings characterId={Number(characterId)} />
-      <div className="px-4">
-        <LocationDemo characterId={Number(characterId)} />
-      </div>
     </div>
   );
 }
